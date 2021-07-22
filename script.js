98004a2ec2bf96472e3ff2f96a58702bf700c8ea
GLouisG <louis.gacho@student.moringaschool.com>
2021-07-22 20:22:37 +0300

    Added text appearance and disappearance functionality for design

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  //user interface logic
+  //for what we do design
+  $(".designimg").click(function(){
+    $(".desp1").toggle(function(){
+      
+    });
+    $(".designimg").toggle(function(){
+
+    });
+  });
+  $(".desp1").click(function(){
+    $(".desp1").toggle(function(){
+      
+    });
+    $(".designimg").toggle(function(){
+
+    });
+  });
+
+
+});
